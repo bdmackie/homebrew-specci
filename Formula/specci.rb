c34b9024b289ab5b1a92626f9d390da73d8b841b
Ben Mackie <ben@mackie.studio>
2025-12-08 23:14:16 +1100

    try again

--- Formula/specci.rb
+++ Formula/specci.rb
@@ -12,10 +12,7 @@ class Specci < Formula
   # The SSH host can be overridden with SPECCI_GITHUB_HOST; default is "github-specci".
   url "https://api.github.com/repos/bdmackie/specci-client/tarball/v0.1.3",
       using: :git,
-      tag: "v0.1.3",
       branch: "main"
-
-  version "HEAD"
 
   # License identifier
   license "Proprietary"
